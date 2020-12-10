17b456951fcaac439214ba10cea19b2964d02923
dragazo <37459229+dragazo@users.noreply.github.com>
2020-12-10 13:04:32 -0600

    store paths + cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::collections::{BTreeSet, BinaryHeap};
+use std::collections::{BTreeSet, BinaryHeap, VecDeque};
 use std::cmp::Ordering;
+use std::str::FromStr;
 use itertools::Itertools;
 
 fn square_dist(a: (isize, isize), b: (isize, isize)) -> usize {
@@ -37,7 +38,7 @@ pub trait Controller {
     fn calculate(&mut self, info: &Info, other: Option<(&Info, &Self::Broadcast)>) -> u8;
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Info {
     pos: (isize, isize),
     dir: u8,
@@ -57,25 +58,31 @@ impl Controller for SimpleController {
     }
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Debug)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
 struct Course {
-    first: Option<u8>,
-    length: usize,
-
+    path: VecDeque<u8>,
     pos: (isize, isize),
     dir: u8,
     target: (isize, isize),
+}
+impl Course {
+    fn info(&self) -> Info {
+        Info { pos: self.pos, dir: self.dir, target: self.target }
+    }
 }
 #[derive(PartialEq, Eq, Debug)]
 struct SortedCoursePair(Course, Course);
 impl SortedCoursePair {
     fn metric(&self) -> usize {
-        let h1 = self.0.length + diamond_dist(self.0.pos, self.0.target);
-        let h2 = self.1.length + diamond_dist(self.1.pos, self.1.target);
+        let h1 = self.0.path.len() + diamond_dist(self.0.pos, self.0.target);
+        let h2 = self.1.path.len() + diamond_dist(self.1.pos, self.1.target);
         h1.max(h2)
     }
     fn len(&self) -> usize {
-        self.0.length.max(self.1.length)
+        self.0.path.len().max(self.1.path.len())
+    }
+    fn info_pair(&self) -> (Info, Info) {
+        (self.0.info(), self.1.info())
     }
 }
 impl Ord for SortedCoursePair {
@@ -89,9 +96,11 @@ impl PartialOrd for SortedCoursePair {
     }
 }
 
-pub struct CompleteController;
+pub struct CompleteController {
+    pre_computed_path: Option<VecDeque<u8>>,
+}
 impl CompleteController {
-    fn plot_path(a: &Info, b: &Info) -> (u8, u8) {
+    fn plot_path(a: &Info, b: &Info) -> (VecDeque<u8>, VecDeque<u8>) {
         debug_assert_ne!(a.pos, b.pos);
         if a.pos < b.pos {
             let r = Self::plot_path(b, a);
@@ -99,24 +108,27 @@ impl CompleteController {
         }
 
         let mut heap: BinaryHeap<SortedCoursePair> = BinaryHeap::with_capacity(1024);
-        let mut heap_set: BTreeSet<(Course, Course)> = Default::default();
-        let mut visited_set: BTreeSet<(Course, Course)> = Default::default();
+        let mut heap_set: BTreeSet<(Info, Info)> = Default::default();
+        let mut visited_set: BTreeSet<(Info, Info)> = Default::default();
         macro_rules! heap_insert {
             ($a:expr, $b:expr $(,)?) => {{
-                let v = ($a, $b);
-                if !visited_set.contains(&v) && heap_set.insert(v) {
+                let v = SortedCoursePair($a, $b);
+                let info = v.info_pair();
+                if !visited_set.contains(&info) && heap_set.insert(info) {
                     heap.push(SortedCoursePair(v.0, v.1))
                 }
             }}
         }
 
         heap_insert!(
-            Course { first: None, length: 0, pos: a.pos, dir: a.dir, target: a.target },
-            Course { first: None, length: 0, pos: b.pos, dir: b.dir, target: b.target },
+            Course { path: Default::default(), pos: a.pos, dir: a.dir, target: a.target },
+            Course { path: Default::default(), pos: b.pos, dir: b.dir, target: b.target },
         );
-        while let Some(SortedCoursePair(a, b)) = heap.pop() {
-            assert!(heap_set.remove(&(a, b))); // take it out of the heap set and put it in the visited set
-            assert!(visited_set.insert((a, b)));
+        while let Some(front) = heap.pop() {
+            let info = front.info_pair();
+            assert!(heap_set.remove(&info)); // take it out of the heap set and put it in the visited set
+            assert!(visited_set.insert(info));
+            let SortedCoursePair(a, b) = front;
 
             if a.pos != a.target && b.pos != b.target {
                 for &da in &[0, 1, 3] {
@@ -124,13 +136,17 @@ impl CompleteController {
                         let new_a_dir = (a.dir + da) % 4;
                         let new_b_dir = (b.dir + db) % 4;
                         if will_collide((a.pos, new_a_dir), (b.pos, new_b_dir)) { continue }
+                        let mut new_a_path = a.path.clone();
+                        let mut new_b_path = b.path.clone();
+                        new_a_path.push_back(da);
+                        new_b_path.push_back(db);
                         let new_a_pos = step(a.pos, new_a_dir);
                         let new_b_pos = step(b.pos, new_b_dir);
-                        if new_a_pos == a.target && new_b_pos == b.target { return (a.first.unwrap_or(da), b.first.unwrap_or(db)); }
+                        if new_a_pos == a.target && new_b_pos == b.target { return (new_a_path, new_b_path); }
 
                         heap_insert!(
-                            Course { first: a.first.or(Some(da)), length: a.length + 1, pos: new_a_pos, dir: new_a_dir, target: a.target },
-                            Course { first: b.first.or(Some(db)), length: b.length + 1, pos: new_b_pos, dir: new_b_dir, target: b.target },
+                            Course { path: new_a_path, pos: new_a_pos, dir: new_a_dir, target: a.target },
+                            Course { path: new_b_path, pos: new_b_pos, dir: new_b_dir, target: b.target },
                         );
                     }
                 }
@@ -139,37 +155,50 @@ impl CompleteController {
                 for &d in &[0, 1, 3] {
                     let new_dir = (a.dir + d) % 4;
                     let new_pos = step(a.pos, new_dir);
-                    if new_pos == a.target { return (a.first.unwrap(), b.first.unwrap()); }
+                    let mut new_path = a.path.clone();
+                    new_path.push_back(d);
+                    if new_pos == a.target { return (new_path, b.path); }
 
-                    heap_insert!(Course { first: a.first, length: a.length + 1, pos: new_pos, dir: new_dir, target: a.target }, b);
+                    heap_insert!(Course { path: new_path, pos: new_pos, dir: new_dir, target: a.target }, b.clone());
                 }
             }
             else if b.pos != b.target {
                 for &d in &[0, 1, 3] {
                     let new_dir = (b.dir + d) % 4;
                     let new_pos = step(b.pos, new_dir);
-                    if new_pos == b.target { return (a.first.unwrap(), b.first.unwrap()); }
+                    let mut new_path = b.path.clone();
+                    new_path.push_back(d);
+                    if new_pos == b.target { return (a.path, new_path); }
 
-                    heap_insert!(a, Course { first: b.first, length: b.length + 1, pos: new_pos, dir: new_dir, target: b.target });
+                    heap_insert!(a.clone(), Course { path: new_path, pos: new_pos, dir: new_dir, target: b.target });
                 }
             }
             else { unreachable!(); }
         }
-        panic!("this should be impossible");
+        unreachable!("if this happend, the proof was wrong");
     }
 }
 impl Controller for CompleteController {
     type Broadcast = ();
-    fn new() -> Self { Self }
+    fn new() -> Self { Self { pre_computed_path: None } }
     fn broadcast(&self) { }
     fn calculate(&mut self, info: &Info, other: Option<(&Info, &Self::Broadcast)>) -> u8 {
-        match other {
-            None => SimpleController.calculate(info, None),
-            Some((other, _)) => CompleteController::plot_path(info, other).0,
+        match &mut self.pre_computed_path {
+            Some(pre) => pre.pop_front().unwrap(),
+            None => match other {
+                None => SimpleController.calculate(info, None),
+                Some((other, _)) => {
+                    let mut r = Self::plot_path(info, other).0;
+                    let v = r.pop_front().unwrap();
+                    self.pre_computed_path = Some(r);
+                    v
+                }
+            }
         }
     }
 }
 
+#[derive(PartialEq, Eq)]
 pub enum SimulationResult {
     Completed,
     Collision,
@@ -216,13 +245,15 @@ impl<T> Simulator<T> where T: Controller {
                     let snapshot = &snapshots[group[0]];
                     Some((&snapshot.0, &snapshot.1))
                 };
-                let delta = self.aircraft[i].1.calculate(&snapshots[i].0, other);
+                let info = &mut self.aircraft[i];
+                let delta = info.1.calculate(&snapshots[i].0, other);
                 assert!(match delta { 0 | 1 | 3 => true, _ => false });
-                self.aircraft[i].0.dir = (self.aircraft[i].0.dir + delta) % 4;
+                info.0.dir = (info.0.dir + delta) % 4; // we can update dir now since everything is cached in snapshots - but changing pos would break collision logic
+                self.paths[info.2].push(step(info.0.pos, info.0.dir)); // add next pos to path before checking for collisions so we can draw failures
             }
 
             for (i, a) in self.aircraft.iter().enumerate() {
-                for b in self.aircraft[i+1..].iter() {
+                for b in self.aircraft[i + 1..].iter() {
                     if will_collide((a.0.pos, a.0.dir), (b.0.pos, b.0.dir)) {
                         return SimulationResult::Collision;
                     }
@@ -232,7 +263,6 @@ impl<T> Simulator<T> where T: Controller {
             for i in (0..self.aircraft.len()).rev() {
                 let info = &mut self.aircraft[i];
                 info.0.pos = step(info.0.pos, info.0.dir);
-                self.paths[info.2].push(info.0.pos);
                 if info.0.pos == info.0.target { self.aircraft.swap_remove(i); }
             }
         }
@@ -286,31 +316,118 @@ fn independent_time(scenario: &[Info]) -> usize {
     max
 }
 
+macro_rules! crash {
+    () => { std::process::exit(1); };
+    ($msg:expr, $($args:expr)*) => {{
+        eprintln!($msg, $($args)*);
+        crash!();
+    }}
+}
+fn parse_int<T: FromStr>(val: &str) -> T {
+    match T::from_str(val) {
+        Ok(v) => v,
+        Err(_) => crash!("failed to parse '{}' as integer of valid range", val),
+    }
+}
+
 fn main() {
-    let mut total = 0u64;
-    let mut nonzero_total = 0u64;
-    let mut delay_rounds = 0u64;
-    let mut max_delay_rounds = 0u64;
-    for_scenarios(8, 2, |i, scenario| {
-        let mut sim = Simulator::<CompleteController>::from_scenario(&scenario);
-        match sim.tick(DEFAULT_SIM_TIME) {
-            SimulationResult::Completed => {
-                let d = (sim.get_time() - independent_time(&scenario)) as u64;
-                total += 1;
-                if d != 0 { nonzero_total += 1; }
-                delay_rounds += d;
-                if d > max_delay_rounds {
-                    max_delay_rounds = d;
-                    println!("new max delay: {} ({:?})", d, scenario);
-                }
-                max_delay_rounds = max_delay_rounds.max(d);
+    let args: Vec<String> = std::env::args().collect();
+    let print_help = || {
+        eprintln!("usage: {} [mode]", args[0]);
+        eprintln!("    mode: help, full, case");
+        
+    };
+
+    if args.len() < 2 {
+        print_help();
+        crash!();
+    }
+    match args[1].as_str() {
+        "-h" | "--help" | "help" => print_help(),
+        "case" => {
+            if args.len() < 5 { crash!(r"usage: {} case controller max_rounds [pos_x:pos_y:dir:target_x:target_y]+
+    controller - simple | complete
+    example: case simple 1000 0:0:1:5:20 10:5:3:-10:0
+", args[0]); }
+            let max_rounds = parse_int(&args[3]);
+            let mut scenario = vec![];
+            for spec in &args[4..] {
+                let v: Vec<isize> = spec.split(':').map(|x| parse_int(x)).collect();
+                if v.len() != 5 { crash!("failed to parse spec '{}'", spec); }
+                if v[2] < 0 || v[2] > 3 { crash!("invalid direction: {} (must be 0-3)", v[2]); }
+                let info = Info { pos: (v[0], v[1]), dir: v[2] as u8, target: (v[3], v[4]) };
+                scenario.push(info);
             }
-            SimulationResult::Collision => panic!("sim {} ({:?}) had a collision", i, scenario),
-            SimulationResult::Running => panic!("sim {} ({:?}) incomplete", i, scenario),
+            fn tick_by<T: Controller>(sim: &mut Simulator<T>, count: usize) -> bool {
+                match sim.tick(count) {
+                    SimulationResult::Collision => { println!("COLLISION!!"); false },
+                    SimulationResult::Running => { println!("INCOMPLETE!!"); false },
+                    SimulationResult::Completed => true,
+                }
+            }
+            match args[2].as_str() {
+                "simple" => {
+                    let mut sim = Simulator::<SimpleController>::from_scenario(&scenario);
+                    let ok = tick_by(&mut sim, max_rounds);
+                    for (i, x) in sim.paths.iter().enumerate() {
+                        println!("path {} - {:?}", i + 1, x);
+                    }
+                    if ok {
+                        println!("{} rounds", sim.get_time());
+                        println!("{} delay rounds", sim.get_time() - independent_time(&scenario));
+                    }
+                }
+                "complete" => {
+                    let mut sim = Simulator::<CompleteController>::from_scenario(&scenario);
+                    let ok = tick_by(&mut sim, max_rounds);
+                    for (i, x) in sim.paths.iter().enumerate() {
+                        println!("path {} - {:?}", i + 1, x);
+                    }
+                    if ok {
+                        println!("{} rounds", sim.get_time());
+                        println!("{} delay rounds", sim.get_time() - independent_time(&scenario));
+                    }
+                }
+                x => crash!("unrecognized controller type: '{}'", x),
+            }
         }
-    });
-    println!("\npassed all {} scenarios", total);
-    println!("max delay: {}", max_delay_rounds);
-    println!("avg delay: {}", delay_rounds as f64 / total as f64);
-    println!("avg nonzero delay: {}", delay_rounds as f64 / nonzero_total as f64);
+        "full" => {
+            if args.len() != 4 { crash!("usage: {} full n k\n    n - size of grid (n x n)\n    k - number of aircraft", args[0]); }
+            let n = parse_int(&args[2]);
+            let k = parse_int(&args[3]);
+
+            let mut count = 0u64;
+            let mut non_trivial_count = 0u64;
+            let mut delay_rounds = 0u64;
+            let mut max_delay_rounds = 0u64;
+            for_scenarios(n, k, |i, scenario| {
+                let mut sim = Simulator::<CompleteController>::from_scenario(&scenario);
+                match sim.tick(DEFAULT_SIM_TIME) {
+                    SimulationResult::Completed => {
+                        let d = (sim.get_time() - independent_time(&scenario)) as u64;
+                        let trivial = Simulator::<SimpleController>::from_scenario(&scenario).tick(DEFAULT_SIM_TIME) == SimulationResult::Completed;
+                        if trivial { assert_eq!(d, 0); } else { non_trivial_count += 1; }
+                        count += 1;
+                        delay_rounds += d;
+                        if d > max_delay_rounds {
+                            max_delay_rounds = d;
+                            println!("new max delay: {} ({:?})", d, scenario);
+                        }
+                        max_delay_rounds = max_delay_rounds.max(d);
+                    }
+                    SimulationResult::Collision => panic!("sim {} ({:?}) had a collision", i, scenario),
+                    SimulationResult::Running => panic!("sim {} ({:?}) incomplete", i, scenario),
+                }
+            });
+            println!("\npassed all {} scenarios ({} non-trivial)", count, non_trivial_count);
+            println!("max delay: {}", max_delay_rounds);
+            println!("avg delay: {}", delay_rounds as f64 / count as f64);
+            println!("avg non-trivial delay: {}", delay_rounds as f64 / non_trivial_count as f64);
+        }
+        x => {
+            eprintln!("unrecognized mode '{}'", x);
+            print_help();
+            crash!();
+        }
+    }
 }
